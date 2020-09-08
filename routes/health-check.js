654e626a1d9ec62a9e0934df52b7179c1cc34497
Sephula Mokeona <sephulam@tihsa.co.za>
2020-09-08 22:51:57 +0200

    added health check route and nodemon package

--- routes/health-check.js
+++ routes/health-check.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/health-check', function(req, res) {
-  res.status(200).json();
+  res.status(200).json({status: "ok"});
 });
 
 module.exports = router;
