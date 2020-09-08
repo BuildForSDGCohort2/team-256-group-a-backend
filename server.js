7842c6fec3603ac67e024ee902554d27039a26f0
Sephula Mokeona <sephulam@tihsa.co.za>
2020-09-08 23:53:00 +0200

    added basic request handling of data coming into register user controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const healthCheck = require("./routes/health-check")  
+const routes = require("./routes/index")
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(healthCheck);
+app.use("/api", routes);
+
 
 app.use(function(req, res, next) {
   next(createError(404));
