5b706e10005c7684e8195a2b36f2dea7cb801505
Kenneth Mokheth <Kennethmahlatsemokhethi@gmail.com>
2020-09-11 04:19:14 +0200

    Fixed issues

--- controller/sign-in.js
+++ controller/sign-in.js
@@ -1,17 +1,14 @@
 const db = require("../db/lowdb");
 
-const sign_in = (req) => {
-  console.log(req.param.email);
-  console.log(req.param.password);
+const getStoredUser = (incommingEmail, incommingPassword) => {
+  return db
+    .get("users")
+    .find({ email: incommingEmail }, { password: incommingPassword })
+    .value();
+};
+const signIn = (req) => {
   let user = getStoredUser(req.param.email, req.param.password);
 
   //If the user is not found return false meaning the user has not registered
-  return user == null ? false : true;
-};
-
-const getStoredUser = (incomming_email, incomming_password) => {
-  return db
-    .get("users")
-    .find({ email: incomming_email }, { password: incomming_password })
-    .value();
+  return user === null ? false : true;
 };
